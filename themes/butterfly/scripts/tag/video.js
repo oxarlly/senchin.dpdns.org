256d87d9092b72176b69782325448128679a5b56
oxarlly <jianhua37212022@163.com>
2026-02-14 22:09:25 +0800

    更新 video.js

--- themes/butterfly/scripts/tag/video.js
+++ themes/butterfly/scripts/tag/video.js
@@ -1,6 +1,6 @@
 hexo.extend.tag.register('video', function (args) {
   const src = args[0]
-  return `
+  return `<div class="videoContent">
 <video class="videoElement" src="${src}" controls preload="auto" width="100%" height="100%"></video>
 </div>
 `
