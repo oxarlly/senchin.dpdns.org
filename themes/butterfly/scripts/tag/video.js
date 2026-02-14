35da4a2a4bbca1bed8a5b53154c05c93194a1b37
oxarlly <jianhua37212022@163.com>
2026-02-14 22:03:09 +0800

    更新 video.js

--- themes/butterfly/scripts/tag/video.js
+++ themes/butterfly/scripts/tag/video.js
@@ -1,6 +1,7 @@
 hexo.extend.tag.register('video', function (args) {
   const src = args[0]
   return `
-<video src="${src}" preload="none" controls width="100%"></video>
+<video class="videoElement" src="${src}" controls preload="auto" width="100%" height="100%"></video>
+</div>
 `
 })
